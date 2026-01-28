87c76da17d73dbd02345512466eb720a67053aee
Hubery Yang <78254474+Hub-yang@users.noreply.github.com>
2026-01-28 17:03:45 +0800

    feat: 更新备案主体号

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,15 +2,15 @@ export default function Footer() {
   return (
     <>
       <div className="footer">
-        <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">京ICP备2023013429号-2</a>
+        <a href="https://beian.miit.gov.cn/" target="_blank" rel="noreferrer">京ICP备2026004664号-1</a>
         &nbsp;
-        <a
+        {/* <a
           href="https://beian.mps.gov.cn/#/query/webSearch?code=11011402054026"
           rel="noreferrer"
           target="_blank"
         >
           京公网安备11011402054026
-        </a>
+        </a> */}
       </div>
     </>
   )
