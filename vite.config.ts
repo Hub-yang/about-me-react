1c412472d0d199d164777a43ea9a69840034488b
Hubery Yang <78254474+Hub-yang@users.noreply.github.com>
2026-08-19 16:04:55 +0800

    chore: 依赖升级，同步更新配置

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import svgr from 'vite-plugin-svgr'
 export default defineConfig({
   resolve: {
     alias: {
-      '~/': `${path.resolve(__dirname, 'src')}/`,
+      '~/': `${path.resolve(import.meta.dirname, 'src')}/`,
     },
   },
   plugins: [react(), svgr()],
